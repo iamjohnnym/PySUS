452d1bed66a712d62d4b8d442c206c1d816e8c01
3cko <j.martin0027@gmail.com>
2013-04-05 13:32:36 -0700

    added methods / script handling

--- phonebook.py
+++ phonebook.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import urllib2
 
 
-#url = "http://pbot.spimageworks.com/pts/jsp/PhoneDirectory.jsp?photo=on&search=malmela&SUBMIT=Go&site=spi&client=spi&proxystylesheet=spi&output=xml_no_dtd"
-
-#response = urllib2.urlopen(url)
-
-#print response.read()
-
-
 class SPIPhonebook():
     def __init__(self):
-        pass
+        self.user_name = None
+        self.results = {}
+        self.html = []
+        self.fields = {'EMAIL:': '<a href='}
 
     def getUrl(self, query):
         url = "http://pbot.spimageworks.com/pts/jsp/PhoneDirectory.jsp?" \
@@ -23,32 +20,54 @@ class SPIPhonebook():
         return url
 
     def getHtml(self, url):
+        """
         file_name = open(url, 'r')
         lines = file_name.readlines()
         file_name.close()
         return lines
         """
         response = urllib2.urlopen(url)
-        return response.read()
-        """
-
-    def parseOptions(self, args):
-        pass
+        for line in response.readlines():
+            for field in self.fields:
+                if field in line:
+                    self.html.append(line)
 
     def parseHtml(self, html):
-        fields = ['EMAIL:']
-        for line in html:
-            for field in fields:
-                if field in line:
-                    print line
+        """
+        Parse Html for specific fields, eg: EMAIL: or WORK:, must correspond
+        with correct tags, self.fields located in __init__.  Example:
 
-    def getField(self, line, element):
-        pass
+        self.fields = {'EMAIL:': '<a href=', 'WORK:': '</span'}
         
+        """
+        for line in html:
+            for field in self.fields:
+                if field in line:
+                    result = self.getField(line, field, self.fields[field])
+                    if not self.results.has_key(field):
+                        self.results[field] = result
+
+    def getField(self, line, field, element):
+        """
+        so ugly, fix me
+        """
+        field_start = line.find(field)
+        field_start = line[field_start:]
+        start = field_start.find(element)
+        start_line = field_start[start:]
+        start_element = start_line.find('>')
+        start_line = start_line[start_element:]
+        end_element = start_line.find('<')
+
+        return start_line[1:end_element].strip()
+    
+    def run(self, user_name):
+        self.user_name = user_name
+        self.getHtml(self.getUrl(user_name))
+        self.parseHtml(self.html)
+        return self.results
 
 
-
-
-pb = SPIPhonebook()
-#b.open_url('FOOOOOOO')
-pb.parseHtml(pb.getHtml('contact.txt'))
+if __name__ == '__main__':
+    pb = SPIPhonebook()
+    print pb.run('jmartin')
